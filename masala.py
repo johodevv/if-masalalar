8a0358edf75ac436d2bf81212cc962ba20e03f9a
johodevv <johodevv@gmail.com>
2026-04-04 16:21:22 +0500

    masalalar

---
+++ masala.py
@@ -0,0 +1,164 @@
+# 10 bilan taqqoslash
+
+
+# number= 10
+# if (number>10):
+#     print(number+3)
+# elif (number<10):
+#     print(number*2)
+# else:    print(22)
+
+# butun qism
+
+
+# a=10
+# b=a//3
+# print(b)
+
+# qoldiqini aniqlash
+
+
+# a=10
+# print(a%3)
+
+# Juft yoki toq son
+
+
+# a=11
+# if(a%2==0):
+#     print("juft son")
+# else:
+#     print("toq son")
+
+# ikkita sondan kattasi
+
+
+# a=4
+# b=35
+# if (a>b):
+#     print("katta son", a)
+# elif (a<b):
+#     print("katta son", b)
+# else:
+#     print("teng")
+
+# a , b , c katta son
+
+
+# a=6
+# b=34
+# c=15
+# if(a>b>c or a>c>b):
+#     print("katta son a=")
+# elif(b>c>a or b>a>c):
+#     print("katta son b=", b)
+# else:
+#     print("katta son c=", c)
+
+# Musbat va Manfiy
+
+
+# a=0
+# if(a>0):
+#     print("musbat")
+# elif(a<0):
+#     print("manfiy")
+# else:
+#     print(0)
+
+# son 5ga bolinadimi
+
+
+# a=34
+# if(a%5==0):
+#     print(True)
+# else:
+#     print(False)
+
+# Son 3 va 4 ga bo'linadimi
+# a=11
+# if(a%3==0 and a%4==0 ):
+#     print(True)
+# else:
+#     print(False)
+
+# Kabisa yil
+
+# yil=2001
+# if(yil%4==0 and yil%100!=0 or yil%400==0 ):
+#     print(True)
+# else:
+#     print(False)
+
+
+# Char sonmi
+
+# a = 9
+
+# if isinstance(a, int):
+#     if 0 <= a <= 9:
+#         print("son")
+# else:
+#     print("alpha")
+
+
+
+# Katta yoki Kichik alfavit
+
+# c=0
+
+# if isinstance(c, str):
+#     if (c==(c.upper())):
+#        print("uppercase")
+#     elif(c==(c.lower())):
+#         print("lowercase")
+# else:
+#     print(None)
+
+# Hafta kuni
+
+# son = 1
+
+# if son == 1: 
+#     print("Dushanba");
+# elif son == 2: 
+#     print("Seshanba");
+# elif son == 3: 
+#     print("Chorshanba");
+# elif son == 4: 
+#     print("Payshanba");
+# elif son == 5: 
+#     print("Juma");
+# elif son == 6: 
+#     print("Shanba");
+# elif son == 7: 
+#     print("Yakshanba");
+# else:
+#     print("none");
+
+# Uchburchakning burchaklari
+
+# a=30
+# b=29
+# c=130
+# if(a+b+c==180):
+#     print(True)
+# else:
+#     print(False)
+
+# Uchburchak tomonlari
+
+# a = 120
+# b = 10 
+# c = 20
+# if(a+b>c and a+c>b and c+b>a):
+#     print(True)
+# else:
+#     print(False)
+
+# Uchburchak shakli
+
+# a = 10
+# b = 15
+# c = 10 
+# if()
